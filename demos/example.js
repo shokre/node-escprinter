1ea138804ef154de22a62a958521591c8f186f2c
shokre <kbrlek@gmail.com>
2014-01-05 17:42:28 +0100

    basic example added

---
+++ demos/example.js
@@ -0,0 +1,24 @@
+
+var EP = require('./../index.js');
+
+var dr = new EP.SVGDriver;
+var p = new EP.Printer(dr);
+
+dr.WriteBeginning();
+var ESC = String.fromCharCode(27);
+var k15 = String.fromCharCode(15);
+var k18 = String.fromCharCode(18);
+
+p.print('normal '+ ESC+'W1WIDE' + ESC + 'W0');
+p.print('\n\r');
+p.print('Normal (PICA)\n\r');
+p.print(ESC+ 'MCompressed (ELITE)\n\r');
+p.print(ESC+ 'P');
+p.print(k15+ 'Ultra Compressed\n\r'+ k18);
+p.print('0....1....2....');
+p.print('0....1....2....');
+p.print('0....1....2....');
+p.print('0....1....2....');
+p.drawMargins();
+dr.WriteEnding();
+dr.save('out.svg');
